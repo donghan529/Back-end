c318a64b976f1348db5f1c26351667ced7b04e54
donghan529 <dong529@naver.com>
2023-09-25 17:42:41 +0900

    0925

---
+++ vscjava/Hello/src/a0925/A0925_8.java
@@ -0,0 +1,17 @@
+package a0925;
+
+public class A0925_8 {
+    public static void main(String[] args) {
+        //정밀도 확인
+        float var1 = 0.1234567890123456789f;
+        double var2 = 0.1234567890123456789;
+        System.out.println(var1);
+        System.out.println(var2);
+        double var3 = 3e6;
+        float var4 = 3e6F;
+        double var5 = 2e-3;
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+    }
+}
