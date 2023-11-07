6ae14a84876fa46627d32416647e6b1136e82f96
donghan529 <dong529@naver.com>
2023-11-07 17:38:30 +0900

    1107

---
+++ spring/1107/firstproject/src/main/java/com/example/firstproject/service/CommentService.java
@@ -0,0 +1,91 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.entity.Comment;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    private final ArticleRepository articleRepository;
+    @Transactional(readOnly = true)
+    public List<CommentDto> comments(Long articleId) {
+        //조회 목록 조회
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+        //변환: 엔티티 -> DTO
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for (int i=0;i<comments.size();i++){
+            Comment comment = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(comment);
+//            CommentDto dto = new CommentDto(comment.getId(), comment.getArticle().getId(), comment.getNickname(), comment.getBody());
+            dtos.add(dto);
+        }
+
+        //결과 확인
+//        for (CommentDto dto: dtos)
+//            System.out.println(dto.toString());
+
+        //stream 방식
+//        List<CommentDto> dtos = comments.stream()
+//                .map(CommentDto::createCommentDto)
+//                .collect(Collectors.toList());
+        //:: 클래스이름::정적메서드이름
+//        return commentRepository.findByArticleId(articleId)
+//                .stream()
+//                .map(comment -> CommentDto.createCommentDto(comment))
+//                .collect(Collectors.toList());
+        //반환
+        return dtos;
+    }
+
+    public CommentDto create(Long articleId, CommentDto dto) {
+       //게시글 조회 및 예외 발생
+       Article article = articleRepository.findById(articleId)
+               .orElseThrow(()-> new IllegalArgumentException("댓글생성실패!"));
+       //댓글 엔티티 생성
+       Comment comment = Comment.createComment(dto, article);
+
+       //댓글 엔티티를 DB에 저장
+       Comment created = commentRepository.save(comment);
+       //Dto로 변경하여 반환
+       return CommentDto.createCommentDto(created);
+    }
+
+    public CommentDto update(Long id, CommentDto dto) {
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("댓글수정실패!"));
+        //댓글수정
+        target.patch(dto);
+
+        //DB로 갱신
+        Comment updated = commentRepository.save(target);
+
+        //댓글 엔티티를 DTO로 변환 및 반환
+        return CommentDto.createCommentDto(updated);
+
+    }
+
+    public CommentDto delete(Long id) {
+        //댓글조회 및 예외발생
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("댓글삭제실패!"));
+        //댓글삭제
+        commentRepository.delete(target);
+
+        //삭제된 댓글을 Dto로 변환
+        return CommentDto.createCommentDto(target);
+    }
+}
---
+++ spring/1107/firstproject/src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,132 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Transactional
+@SpringBootTest //해당 클래스는 스프링부트와 연동되어 테스팅된다.
+class ArticleServiceTest {
+
+    @Autowired
+    ArticleService articleService;
+
+    @Test   //해당 메서드가 테스트를 위한 코드라는 것을 선언하는 어노테이션
+    void index() {  //성공과 실패의 경우를 한가지 테스트에서 실행
+        //예상시나리오
+        Article a = new Article(1L, "가가가가", "1111");
+        Article b = new Article(2L, "나나나나", "2222");
+        Article c = new Article(3L, "다다다다", "3333");
+        List<Article> expected = new ArrayList<>(Arrays.asList(a,b,c));
+        //실제 결과
+        List<Article> articles = articleService.index();
+        //비교(예상 시나리오와 실제 결과가 같은지 비교한다.)
+        assertEquals(expected.toString(),articles.toString());
+
+    }
+
+    @Test
+    void show_성공() {//존재하는 id 입력
+        //예상
+        long id  = 1L;
+        Article expected = new Article(id, "가가가가", "1111");
+        //실제
+        Article article = articleService.show(id);
+        //비교
+        assertEquals(expected.toString(),article.toString());
+
+    }
+    @Test
+    void show_실패() {//존재할 수 없는 id 입력
+        //예상
+        long id  = -1L;
+        Article expected = null;
+        //실제
+        Article article = articleService.show(id);
+        //비교
+        assertEquals(expected,article);//null값은 toString 메서드 사용 X
+
+    }
+
+    @Test
+    void create_성공() {//title과 content만 있는 dto입력
+        //예상
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(null, title, content);
+        Article expected = new Article(4L, title, content);
+
+        //실제
+        Article article = articleService.create(dto);
+        //비교
+        assertEquals(expected.toString(),article.toString());
+        System.out.println("예상결과: " + expected.toString());
+        System.out.println("실제결과: " + article.toString());
+    }
+    @Test
+    void create_실패() {//id가 포함된 dto입력
+        //예상
+        String title = "라라라라";
+        String content = "4444";
+        ArticleForm dto = new ArticleForm(4L, title, content);
+        Article expected = null;
+
+        //실제
+        Article article = articleService.create(dto);
+        //비교
+        assertEquals(expected,article);
+
+    }
+
+    @Test
+    void update_성공() {
+        //예상
+        Long id = 1L;
+        ArticleForm dto = new ArticleForm(id,"가나가나가나","121212");
+        Article expected = new Article(id, "가나가나가나", "121212");
+        //실제
+        Article article = articleService.update(id, dto);
+        //비교
+        assertEquals(expected.toString(),article.toString());
+    }
+    @Test
+    void update_실패() {
+        //예상
+        Long id = 1L;
+        ArticleForm dto = new ArticleForm(id,"가나가나가나","121212");
+        Article expected = null;
+        //실제
+        Article article = articleService.update(-1L, dto);
+        //비교
+        assertNull(article);
+    }
+
+    @Test
+    void delete_성공() {
+        //예상
+        Long id = 1L;
+        Article expected = new Article(id, "가가가가", "1111");
+        //실제
+        Article article = articleService.delete(id);
+        //비교
+        assertEquals(expected.toString(), article.toString());
+    }
+    @Test
+    void delete_실패() {
+        //예상
+        Long id = -1L;
+        Article expected = null;
+        //실제
+        Article article = articleService.delete(id);
+        //비교
+        assertNull(article);
+    }
+}
