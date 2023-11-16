feaf7f93407a3effc5b69870f1bd65b9438c5bbf
donghan529 <dong529@naver.com>
2023-11-16 17:11:08 +0900

    1116

---
+++ spring/1116/shop/src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,43 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "item")
+@Getter
+@Setter
+@ToString
+public class Item {
+
+    @Id
+    @Column(name = "item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;                        //상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm;                  //상품명
+    //null 허용 하지 않고 - itemNm 필드는 항상 값을 가져야 한다. 최대 50자
+
+    @Column(name = "price", nullable = false)
+    private int price;                      //가격
+
+    @Column(nullable = false)
+    private int stockNumber;                //재고 수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;              //상품 상세 설명
+    //@Lob - 대용량의 데이터를 저장
+
+    private ItemSellStatus itemSellStatus;  //상품 판매 상태
+
+    private LocalDateTime regTime;          //등록 시간
+
+    private LocalDateTime updateTime;       //수정 시간
+}
