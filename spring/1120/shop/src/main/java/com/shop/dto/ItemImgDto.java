2a062c0bdccdfb3448f1b2df1cd27c94d86aac15
donghan529 <dong529@naver.com>
2023-11-20 17:41:52 +0900

    1120

---
+++ spring/1120/shop/src/main/java/com/shop/dto/ItemImgDto.java
@@ -0,0 +1,40 @@
+package com.shop.dto;
+
+import com.shop.entity.ItemImg;
+import lombok.Getter;
+import lombok.Setter;
+import org.modelmapper.ModelMapper;
+
+@Getter @Setter
+public class ItemImgDto {
+    private Long id;
+
+    private String imgName;
+
+    private String oriImgName;
+
+    private String imgUrl;
+
+    private String repImgYn;
+
+    private static ModelMapper modelMapper = new ModelMapper();
+
+    private static ItemImgDto of(ItemImg itemImg) { //or 변환메서드에서 사용
+        return modelMapper.map(itemImg, ItemImgDto.class);
+    } //map 소스메소드 ItemImg 소스객체를 대상클래스(ItemImgDto.class)를 인자로 받아서 변환 수행
+    //static 메소드로 선언해서 ItemImgDto를 생성하지 않아도 호출 할 수 있도록 한다.
+
+    //ModelMapper를 사용하여 ItemImg 엔티티를 ItemImgDto로 변환하는 정적메서드
+
+//    public ItemImgDto() {
+//        // 기본 생성자
+//    }
+
+//    public ItemImgDto(ItemImg itemImg) {
+//        this.id = itemImg.getId();
+//        this.imgName = itemImg.getImgName();
+//        this.oriImgName = itemImg.getOriImgName();
+//        this.imgUrl = itemImg.getImgUrl();
+//        this.repImgYn = itemImg.getRepImgYn();
+//    }
+}
