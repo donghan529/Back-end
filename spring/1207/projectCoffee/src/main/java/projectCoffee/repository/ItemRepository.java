24844b1f40b6b462828fd1a9999a19771e723238
donghan529 <dong529@naver.com>
2023-12-07 17:39:29 +0900

    1207

--- spring/1207/projectCoffee/src/main/java/projectCoffee/repository/ItemRepository.java
+++ spring/1207/projectCoffee/src/main/java/projectCoffee/repository/ItemRepository.java
@@ -1,25 +1,29 @@
-package com.shop.repository;
+package projectCoffee.repository;
 
-import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
+import projectCoffee.entity.Item;
 
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long>,
-        QuerydslPredicateExecutor<Item>, ItemRepositoryCustom {
+        QuerydslPredicateExecutor<Item> {
     List<Item> findByItemNm(String itemNm);
-    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm,String itemDetail);
+
     List<Item> findByPriceLessThan(Integer price);
+
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
     @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
     List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
-    @Query(value="select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+
+    @Query(value = "select * from item i where i.item_detail lick %:itemDetail% order by i.price desc", nativeQuery = true)
     List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
 
-//    @Query(value="select row_number() over(order by article_id desc) as num, article_id, article_type, title, content from projectshop.article", nativeQuery = true)
-//    List<Item> findByNumByNative(@Param("num") int num);
+
 }
+
